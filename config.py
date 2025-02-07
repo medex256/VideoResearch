c2c2c9433d6f6e8f2b4a701ac423f7f71540d56d
Madi Kaiyrkhan <madicairchan@gmail.com>
2025-02-07 15:28:37 +0000

    database host

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ app  = Flask(__name__)
 
 app.secret_key = "secret-key-here"
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///:memory:"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlitecloud://cvrm0nakhz.g4.sqlite.cloud:8860/chinook.sqlite?apikey=28LhdU8oT71PsIIEqoFcgHgl5kL3NQm1Is6IqYHdhAI"
 
 
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
