b7914b15fdf0b53f9c8d217437932ce1e3cebad7
Madi Kaiyrkhan <madicairchan@gmail.com>
2025-02-07 15:55:11 +0000

    mysql test

--- config.py
+++ config.py
@@ -2,13 +2,15 @@ from flask import Flask
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
+import sqlitecloud
+
 
 
 app  = Flask(__name__)
 
 app.secret_key = "secret-key-here"
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlitecloud://cvrm0nakhz.g4.sqlite.cloud:8860/chinook.sqlite?apikey=28LhdU8oT71PsIIEqoFcgHgl5kL3NQm1Is6IqYHdhAI"
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://u3dmiywrwj3ignqx:nbb2d11TheFaD67PbM50@bwdnizuvr3xuibcrnls5-mysql.services.clever-cloud.com:3306/bwdnizuvr3xuibcrnls5"
 
 
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
