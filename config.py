7f759ffa3189b6411383f7649c9fddf5127accf9
Madi Kaiyrkhan <madicairchan@gmail.com>
2025-07-14 21:19:44 +0800

    optmize db

--- config.py
+++ config.py
@@ -15,7 +15,7 @@ app.secret_key = os.environ.get("SECRET_KEY", "secret-key-here")
 # Database configuration
 # -----------------------------------------------------------------------------
 # Default to SQLite in current folder if DATABASE_URL is not set
-default_sqlite = f"sqlite:///{os.path.join(os.getcwd(), 'mydatabase.db')}"
+default_sqlite = "sqlite:///mydatabase.db"
 database_url = os.environ.get("DATABASE_URL", default_sqlite)
 app.config["SQLALCHEMY_DATABASE_URI"] = database_url
 
@@ -25,8 +25,8 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 # If using MySQL (i.e. RDS), add a connection pool
 if database_url.startswith(("mysql://", "mysql+pymysql://")):
     app.config["SQLALCHEMY_ENGINE_OPTIONS"] = {
-        "pool_size":      int(os.environ.get("DB_POOL_SIZE", 10)),
-        "max_overflow":   int(os.environ.get("DB_MAX_OVERFLOW", 20)),
+        "pool_size":      int(os.environ.get("DB_POOL_SIZE", 20)),
+        "max_overflow":   int(os.environ.get("DB_MAX_OVERFLOW", 40)),
         "pool_recycle":   int(os.environ.get("DB_POOL_RECYCLE", 300)),
         "pool_pre_ping":  True,
     }
